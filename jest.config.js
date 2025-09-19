fcec9b8e62f67eff560c8cffeafb2aa24af50cf7
Zazzik1 <mateuszbilicki34@gmail.com>
2025-09-20 01:42:32 +0200

    fix: unit test config

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,7 @@
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node'
-}
+    preset: 'ts-jest',
+    testEnvironment: 'node',
+    transform: {
+        '^.+\\.(ts|tsx)$': ['ts-jest', { tsconfig: 'tsconfig.jest.json' }],
+    },
+};
