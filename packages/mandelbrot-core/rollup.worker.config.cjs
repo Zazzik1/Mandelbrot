ade271d3e75d7beb7bd9cfab52e901b59b7872f9
Zazzik1 <mateuszbilicki34@gmail.com>
2025-09-30 11:11:45 +0200

    added note about license in bundles

--- packages/mandelbrot-core/rollup.worker.config.cjs
+++ packages/mandelbrot-core/rollup.worker.config.cjs
@@ -1,10 +1,17 @@
 const typescript = require('@rollup/plugin-typescript');
+const pkg = require('./package.json');
 
 module.exports = {
     input: 'src/workers/mandelbrot.worker.ts',
     output: {
         file: 'dist/workers/mandelbrot.worker.js',
         format: 'esm',
+        banner: `/*!
+ * ${pkg.name} v${pkg.version}
+ * (c) ${new Date().getFullYear()} ${pkg.author}
+ * License: ${pkg.license}
+ */
+`,
     },
     plugins: [typescript({ tsconfig: './tsconfig.rollup.json' })],
 };
